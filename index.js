21d0ff9883d319ee0af3cdcf17565e95b47f8e0f
NikwaF <nikonabilah@gmail.com>
2020-04-18 13:29:38 +0700

    wrong routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-require(`${__dirname}/routes/dialogFLowRoutes`)(app);
+require(`${__dirname}/routes/dialogFlowRoutes`)(app);
 
 
 
